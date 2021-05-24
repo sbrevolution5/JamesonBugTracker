914494d548b67ba4d220daa20697484f611e0524
sbrevolution5 <sbrevolution5@aim.com>
2021-05-24 15:22:27 -0400

    Add project files.

---
+++ JamesonBugTracker/Models/BTUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JamesonBugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName
+        {
+            get
+            { return FirstName + LastName; }
+        }
+    }
+}
---
+++ JamesonBugTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JamesonBugTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
