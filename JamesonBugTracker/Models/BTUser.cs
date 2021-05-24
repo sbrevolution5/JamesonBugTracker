3e10f43d14b7a705a96dd7fbfd9acb4bcee95f97
sbrevolution5 <sbrevolution5@aim.com>
2021-05-24 15:42:24 -0400

    Update to Data annotations

--- JamesonBugTracker/Models/BTUser.cs
+++ JamesonBugTracker/Models/BTUser.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,13 +11,20 @@ namespace JamesonBugTracker.Models
 {
     public class BTUser : IdentityUser
     {
+        [Required]
+        [DisplayName("First Name")]
+        [StringLength(50)]
         public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        [StringLength(50)]
         public string LastName { get; set; }
         [NotMapped]
+        [DisplayName("Full Name")]
         public string FullName
         {
             get
-            { return FirstName + LastName; }
+            { return $"{FirstName} + {LastName}"; }
         }
     }
 }
