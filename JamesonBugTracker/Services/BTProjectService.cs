13c4b23ec8a6b2079aeb09c1922100f7ee1acb25
sbrevolution5 <sbrevolution5@aim.com>
2021-05-27 21:20:52 -0400

    initial work on project service, removing antonio's traps

--- JamesonBugTracker/Services/BTProjectService.cs
+++ JamesonBugTracker/Services/BTProjectService.cs
@@ -1,5 +1,7 @@
-﻿using JamesonBugTracker.Models;
+﻿using JamesonBugTracker.Data;
+using JamesonBugTracker.Models;
 using JamesonBugTracker.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,54 +11,126 @@ namespace JamesonBugTracker.Services
 {
     public class BTProjectService : IBTProjectService
     {
-        public Task<bool> AddProjectManagerAsync(string userId, int projectId)
+        private readonly ApplicationDbContext _context;
+        private readonly IBTCompanyInfoService _companyInfoService;
+        private readonly IBTRolesService _rolesService;
+
+        public BTProjectService(ApplicationDbContext context, IBTCompanyInfoService companyInfoService)
+        {
+            _context = context;
+            _companyInfoService = companyInfoService;
+        }
+        /// <summary>
+        /// returns false if project has a manager, or supplied user is not a manager.  Otherwise adds manager and returns true
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="projectId"></param>
+        /// <returns></returns>
+        public async Task<bool> AddProjectManagerAsync(string userId, int projectId)
+        {
+            //if the user supplied isn't a manager, return false
+            BTUser newManager = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            if (!await _rolesService.IsUserInRoleAsync(newManager, "ProjectManager"))
+            {
+                return false;
+            }
+            var project = await _context.Project.FirstOrDefaultAsync(p => p.Id == projectId);
+            //This returns a user who is a member, or it returns null?
+            foreach (var user in project.Members)
+            {
+                // If user is a project manager, return false.  
+                if (await _rolesService.IsUserInRoleAsync(user, "ProjectManager"))
+                {
+                    return false;
+                }
+            }
+            //else, assign the new project manager to the project, then return true
+            project.Members.Add(newManager);
+            return true;
+        }
+
+        public async Task<bool> AddUserToProjectAsync(string userId, int projectId)
+        {
+            var project = await _context.Project.FirstOrDefaultAsync(p => p.Id == projectId);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            project.Members.Add(user);
+            var res = await _context.SaveChangesAsync();
+            if (res >= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<List<BTUser>> DevelopersOnProjectAsync(int projectId)
+        {
+            return await GetProjectMembersByRoleAsync(projectId, "Developer");
+        }
+
+        public async Task<List<Project>> GetAllProjectsByCompanyAsync(int companyId)
+        {
+            var companyProjects = (await _companyInfoService.GetAllProjectsAsync(companyId)).ToList();
+            return companyProjects;
+
+        }
+
+        public Task<List<Project>> GetAllProjectsByPriorityAsync(int companyId, string priorityName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> AddUserToProjectAsync(string userId, int projectId)
+        public async Task<List<Project>> GetArchivedProjectsByCompanyAsync(int companyId)
         {
-            throw new NotImplementedException();
+            var companyProjects = await GetAllProjectsByCompanyAsync(companyId);
+            var archivedCompanyProjects = companyProjects.Where(p => p.Archived).ToList();
+            return archivedCompanyProjects;
         }
 
-        public Task<List<BTUser>> DevelopersOnProjectAsync(int projectId)
+        public async Task<List<BTUser>> GetMembersWithoutPMAsync(int projectId)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Project>> GetAllProjectsByCompany(int companyId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<BTUser>> GetMembersWithoutPMAsync(int projectId)
-        {
-            throw new NotImplementedException();
+            var project = await _context.Project.FirstOrDefaultAsync(p => p.Id == projectId);
+            List<BTUser> nonPMUsers = new();
+            foreach (var user in project.Members)
+            {
+                if (!await _rolesService.IsUserInRoleAsync(user, "ProjectManager"))
+                {
+                    nonPMUsers.Add(user);
+                }
+            }
+            return nonPMUsers;
         }
 
         public Task<BTUser> GetProjectManagerAsync(int projectId)
         {
+            //What if there isn't one?!
             throw new NotImplementedException();
         }
 
-        public Task<List<BTUser>> GetProjectMembersByRoleAsync(int projectId, string role)
+        public async Task<List<BTUser>> GetProjectMembersByRoleAsync(int projectId, string role)
         {
-            throw new NotImplementedException();
+            var project = await _context.Project.FirstOrDefaultAsync(p => p.Id == projectId);
+            List<BTUser> membersByRole = new();
+            foreach (var user in project.Members)
+            {
+                if (await _rolesService.IsUserInRoleAsync(user, role))
+                {
+                    membersByRole.Add(user);
+                }
+            }
+            return membersByRole;
         }
 
-        public Task<bool> IsUserOnProject(string userId, int projectId)
+        public async Task<bool> IsUserOnProjectAsync(string userId, int projectId)
         {
-            throw new NotImplementedException();
+            var project = await _context.Project.FirstOrDefaultAsync(p => p.Id == projectId);
+            foreach (var user in project.Members)
+            {
+                if (user.Id == userId)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public Task<List<Project>> ListUserProjectsAsync(string userId)
@@ -74,7 +148,7 @@ namespace JamesonBugTracker.Services
             throw new NotImplementedException();
         }
 
-        public Task RemoveUsersFromProjectByRoleAsync(string userId, int projectId)
+        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId)
         {
             throw new NotImplementedException();
         }
--- JamesonBugTracker/Services/Interfaces/IBTProjectService.cs
+++ JamesonBugTracker/Services/Interfaces/IBTProjectService.cs
@@ -8,21 +8,21 @@ namespace JamesonBugTracker.Services.Interfaces
 {
     public interface IBTProjectService
     {
-        public Task<bool> IsUserOnProject(string userId, int projectId);
+        public Task<bool> IsUserOnProjectAsync(string userId, int projectId);
 
         public Task<bool> AddUserToProjectAsync(string userId, int projectId);
 
         public Task RemoveUserFromProjectAsync(string userId, int projectId);
 
-        public Task RemoveUsersFromProjectByRoleAsync(string userId, int projectId);
+        public Task RemoveUsersFromProjectByRoleAsync(string role, int projectId);
 
         public Task<List<Project>> ListUserProjectsAsync(string userId);
 
-        public Task<List<Project>> GetAllProjectsByCompany(int companyId);
+        public Task<List<Project>> GetAllProjectsByCompanyAsync(int companyId);
 
-        public Task<List<Project>> GetArchivedProjectsByCompany(int companyId);
+        public Task<List<Project>> GetArchivedProjectsByCompanyAsync(int companyId);
 
-        public Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName);
+        public Task<List<Project>> GetAllProjectsByPriorityAsync(int companyId, string priorityName);
 
         public Task<List<BTUser>> GetProjectMembersByRoleAsync(int projectId, string role);
 
