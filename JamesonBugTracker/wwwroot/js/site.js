d10268dc197312dedf8f92e997f7c2c5b93ff114
sbrevolution5 <sbrevolution5@aim.com>
2021-06-03 11:29:23 -0400

    attempted to add dark mode toggler

--- JamesonBugTracker/wwwroot/js/site.js
+++ JamesonBugTracker/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Toggles dark mode
+function toggleDark() {
+    var element = document.getElementById("layoutBody")
+    element.classList.toggle("dark-mode")
+    let dark = JSON.parse(localStorage.getItem("jamesonDarkMode")) 
+    if (dark) {
+        localStorage.setItem("jamesonDarkMode", JSON.stringify(false))
+        console.log("Dark mode off")
+    }
+    else {
+        localStorage.setItem("jamesonDarkMode", JSON.stringify(true))
+        console.log("Dark mode on")
 
-// Write your JavaScript code.
+    }
+}
+//Checks localstorage for dark mode toggler obj, and creates one if it isn't there
+function loadDark() {
+    //default is light mode
+    console.log("dark mode is ", JSON.parse(localStorage.getItem("jamesonDarkMode")))
+    let dark = JSON.parse(localStorage.getItem("jamesonDarkMode")) || "false"
+    if (dark == false) {
+        localStorage.setItem("jamesonDarkMode", JSON.stringify(false))
+
+    }
+    else {
+        toggleDark()
+    }
+}
