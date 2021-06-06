f82e8f52cd466ead8bcf76daa6c6b116539fd5dc
sbrevolution5 <sbrevolution5@aim.com>
2021-06-06 12:51:33 -0400

    chart colors

--- JamesonBugTracker/wwwroot/js/ProjectDetails.js
+++ JamesonBugTracker/wwwroot/js/ProjectDetails.js
@@ -1,35 +1,38 @@
 ﻿var backgroundColor = ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de']
+var textColor
 function darkSwap() {
     var isDark = document.getElementById("layoutBody").classList.contains("dark-mode")
     if (isDark) {
-        backgroundColor = ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de']
+        //Dark colors RED Orange YELLOW Green blue (primary) 
+        backgroundColor = ['#e74b3c', '#fd7e14', '#f39c12', '#06BC8C', '#3f6791']
+        textColor = '#ffffff'
     } else {
-        backgroundColor = ['#ffffff', '#000000', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de']
+        //Light colors
+        backgroundColor = ['#dc3545', '#fd7e14', '#ffc108', '#27a745', '#1385ff', '#3c8dbc', '#fd7e14']
+        textColor = '#202529'
+
     }
-    console.log("bg color changed")
     charts()
 }
-function charts(){//Priority Chart JS
+function charts() {//Priority Chart JS
     var donutChartCanvas = $('#priorityChart').get(0).getContext('2d')
     var donutData = {
         labels: [
-            'Chrome',
-            'IE',
-            'FireFox',
-            'Safari',
-            'Opera',
-            'Navigator',
+            'Urgent',
+            'High',
+            'Medium',
+            'Low'
         ],
         datasets: [
             {
-                data: [700, 500, 400, 600, 300, 100],
+                data: priority,
                 backgroundColor: backgroundColor,
             }
         ]
     }
     var donutOptions = {
         maintainAspectRatio: false,
-        responsive: true,
+        responsive: true
     }
     new Chart(donutChartCanvas, {
         type: 'doughnut',
@@ -42,16 +45,14 @@ function charts(){//Priority Chart JS
     var donutChartCanvas = $('#typeChart').get(0).getContext('2d')
     var donutData = {
         labels: [
-            'Chrome',
-            'IE',
-            'FireFox',
-            'Safari',
-            'Opera',
-            'Navigator',
+            'New Development',
+            'Runtime',
+            'UI',
+            'Maintenance'
         ],
         datasets: [
             {
-                data: [700, 500, 400, 600, 300, 100],
+                data: types,
                 backgroundColor: backgroundColor,
             }
         ]
@@ -71,16 +72,15 @@ function charts(){//Priority Chart JS
     var donutChartCanvas = $('#statusChart').get(0).getContext('2d')
     var donutData = {
         labels: [
-            'Chrome',
-            'IE',
-            'FireFox',
-            'Safari',
-            'Opera',
-            'Navigator',
+            'New',
+            'Unassigned',
+            'Testing',
+            'Development',
+            'Resolved',
         ],
         datasets: [
             {
-                data: [700, 500, 400, 600, 300, 100],
+                data: statuses,
                 backgroundColor: backgroundColor,
             }
         ]
