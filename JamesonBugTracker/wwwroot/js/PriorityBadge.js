deb5c1acd2a68a8d3d2433330f3913b4788001ab
sbrevolution5 <sbrevolution5@aim.com>
2021-06-07 09:39:48 -0400

    conditionalFormatting for project Badges also adding correct table elements

---
+++ JamesonBugTracker/wwwroot/js/PriorityBadge.js
@@ -0,0 +1,22 @@
+﻿function ticketPriLabelColor(e) {
+    let priority = e.innerText
+    let ticketPri = "";
+    if (priority == "Urgent") {
+        ticketPri = "badge-danger";
+    }
+    else if (priority == "High") {
+        ticketPri = "badge-orange";
+    }
+    else if (priority == "Medium") {
+        ticketPri = "badge-warning";
+    }
+    else if (priority == "Low") {
+        ticketPri = "badge-success";
+    }
+    e.classList.toggle(ticketPri)
+}
+
+let badges = document.getElementsByClassName("priorityBadge")
+for (let item of badges) {
+    ticketPriLabelColor(item)
+}
