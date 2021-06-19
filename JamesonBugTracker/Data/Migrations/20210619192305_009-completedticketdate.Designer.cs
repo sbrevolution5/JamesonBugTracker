d8290a76a3ec4e1b3f7890035f3c09fa4fee2187
sbrevolution5 <sbrevolution5@aim.com>
2021-06-19 15:23:33 -0400

    oops forgot migration

--- JamesonBugTracker/Data/Migrations/20210619192305_009-completedticketdate.Designer.cs
+++ JamesonBugTracker/Data/Migrations/20210619192305_009-completedticketdate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using JamesonBugTracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace JamesonBugTracker.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210619192305_009-completedticketdate")]
+    partial class _009completedticketdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JamesonBugTracker/Data/Migrations/20210619192305_009-completedticketdate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JamesonBugTracker.Data.Migrations
+{
+    public partial class _009completedticketdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Ticket",
+                type: "character varying(75)",
+                maxLength: 75,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Completed",
+                table: "Ticket",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Ticket");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Ticket",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(75)",
+                oldMaxLength: 75);
+        }
+    }
+}
